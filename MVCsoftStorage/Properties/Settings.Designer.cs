a1321fcab1810995a19cac682df6a69f79749d5c
Uvix Bag <baguvix88@outlook.com>
2018-12-09 16:22:02 +0300

    deploy

--- MVCsoftStorage/Properties/Settings.Designer.cs
+++ MVCsoftStorage/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace MVCsoftStorage.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.8.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -26,7 +26,8 @@ namespace MVCsoftStorage.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("server=localhost;user id=root;database=mydb")]
+        [global::System.Configuration.DefaultSettingValueAttribute("server=localhost;user id=root;password=root;database=storage;port=3306;persistsec" +
+            "urityinfo=True")]
         public string ConnectionString {
             get {
                 return ((string)(this["ConnectionString"]));
